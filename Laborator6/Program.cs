c7c7d1cb78ea8834cec65d74f6c9900348d78d28
Patroi Alex <67483871+Alex99P@users.noreply.github.com>
2021-11-21 22:22:26 +0200

    Add files via upload

---
+++ Laborator6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laborator6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
